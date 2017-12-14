b20e7399c272e062ece1415027b335b398bcb0a2
Harish <harish92.maram@gmail.com>
2017-12-13 23:38:18 -0800

    first commit

---
+++ src/main/java/scloud/demo/SpringcloudM1ConfigserverGitApplication.java
@@ -0,0 +1,14 @@
+package scloud.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class SpringcloudM1ConfigserverGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudM1ConfigserverGitApplication.class, args);
+	}
+}
